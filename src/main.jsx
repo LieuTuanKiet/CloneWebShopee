ccc645f9096a84721d1abb19a1e522c1132d04bd
LieuTuanKiet <Redboy71784@gmail.com>
2025-08-13 16:55:07 +0700

    chore: setup vite react and tailwind for project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './main.css'
+import MainPage from './pages/MainPage'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <MainPage />
+  </StrictMode>,
+)
